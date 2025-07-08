e6c4f1fc8c2af1ba925981c2be0be2e2e49208ba
skandarchahbouni <js_chahbouni@esi.dz>
2025-07-08 12:14:42 +0200

    protecting valkey password

--- db/valkey/config.py
+++ db/valkey/config.py
@@ -1,4 +1,8 @@
 import redis
+import os
+
+
+VALKEY_PASSWORD = os.getenv("VALKEY_PASSWORD")
 
 # Connect to Redis
-valkey_client = redis.Redis(host='localhost', port=6379, db=0, decode_responses=True)
+valkey_client = redis.Redis(host='localhost', port=6379, db=0, password=VALKEY_PASSWORD, decode_responses=True)
